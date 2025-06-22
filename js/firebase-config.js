0c4b7c1215a29bfd541ca44870715f3a372baae4
FRznck <franckkouassi4038@gmail.com>
2025-06-22 12:23:44 +0200

    test

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAjZuld7cIh6uXyuiH83Xj6p3EeIVhilHE", 
+    authDomain: "asten-tickets.firebaseapp.com",
+    projectId: "asten-tickets",
+    storageBucket: "asten-tickets.firebasestorage.app",
+    messagingSenderId: "474907811866",
+    appId: "1:474907811866:web:fd80fa99140ea320a1b1a3"
+}; 
